f622b585686ae0eb2fb5feec82e2dfc422328a44
CienZZZ <Krzysztofkan@poczta.fm>
2019-12-19 12:42:49 +0100

    first working UserControllerTest unauthorized

--- src/test/java/pl/weilandt/wms/user/UserControllerTest.java
+++ src/test/java/pl/weilandt/wms/user/UserControllerTest.java
@@ -1,20 +1,41 @@
 package pl.weilandt.wms.user;
 
-//@SpringBootTest
-//@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
-//    @MockBean
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private UserController userController;
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    public void whenUserControllerInjected_thenNotNull() throws Exception {
-//        assertThat(userController).isNotNull();
-//    }
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private UserDTO userDTO;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    @Test
+    public void unauthorized() throws Exception {
+        mockMvc.perform(post("/users/all")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
 }
